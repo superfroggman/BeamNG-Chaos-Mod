36d6a79891d0038e274b6dcd423a0578af5e481f
superfroggman <49985901+superfroggman@users.noreply.github.com>
2021-01-23 18:37:50 +0100

    print possible vehicle commands

--- test.lua
+++ test.lua
@@ -1,2 +1,7 @@
+-- GOOD TO HAVE!: veh:queueLuaCommand("for k,v in pairs(powertrain.getDevice(\"mainEngine\")) do print(k) end")
+
+
 local veh = scenetree.findObject(be:getPlayerVehicleID(0))
 print(veh:queueLuaCommand("print(\"Hello World\")")) 
+
+veh:queueLuaCommand("for k,v in pairs(powertrain.getDevice(\"mainEngine\")) do print(k) end")
