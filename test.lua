ac851be0681ffc19e77920637ed4b4f5038e441b
superfroggman <49985901+superfroggman@users.noreply.github.com>
2021-01-23 19:02:30 +0100

    figured out how to run vehicle LUA in GE lua

--- test.lua
+++ test.lua
@@ -5,3 +5,5 @@ local veh = scenetree.findObject(be:getPlayerVehicleID(0))
 print(veh:queueLuaCommand("print(\"Hello World\")")) 
 
 veh:queueLuaCommand("for k,v in pairs(powertrain.getDevice(\"mainEngine\")) do print(k) end")
+
+veh:queueLuaCommand("powertrain.getDevice(\"mainEngine\").thermals.engineBlockTemperature = 50")
